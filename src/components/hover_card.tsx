2f33681547a2bb9435bd1098a9398ed950331bc8
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2024-02-29 19:38:13 -0500

    working hover card!

--- src/components/hover_card.tsx
+++ src/components/hover_card.tsx
@@ -1,10 +1,10 @@
-import { CalendarIcon } from "@radix-ui/react-icons";
-
+import { Button } from "@/components/ui/button";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
 
 export function TranslationHoverCard({
   children,
@@ -15,19 +15,15 @@ export function TranslationHoverCard({
     <HoverCard>
       <HoverCardTrigger asChild>{children}</HoverCardTrigger>
       <HoverCardContent className="w-80">
-        <div className="flex justify-between space-x-4">
-          <div className="space-y-1">
-            <h4 className="text-sm font-semibold">@nextjs</h4>
-            <p className="text-sm">
-              The React Framework – created and maintained by @vercel.
-            </p>
-            <div className="flex items-center pt-2">
-              <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "}
-              <span className="text-xs text-muted-foreground">
-                Joined December 2021
-              </span>
-            </div>
+        <div className="space-y-1">
+          <div className="flex justify-between">
+            <h4 className="text-sm font-semibold self-center">Translation</h4>
+            <Button variant="ghost" size="icon">
+              <PlusCircledIcon className="h-5 w-5" />
+            </Button>
           </div>
+          <p className="text-sm">This is the token definition.</p>
+          <div className="flex items-center pt-2"></div>
         </div>
       </HoverCardContent>
     </HoverCard>
