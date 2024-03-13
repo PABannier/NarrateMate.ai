215153eae2517de208fbee170c6f1463662bb583
Justina <justcho5@gmail.com>
2024-03-12 18:32:50 -0700

    added unstable_cache to getall function

--- src/lib/database/queries.ts
+++ src/lib/database/queries.ts
@@ -6,27 +6,32 @@ import {
 import { getYouTubeVideoTitle } from "../youtube";
 import { convertKeysToCamelCase } from "../utils";
 import { cache } from "react";
-const getAllFromDB = cache(async (supabase: SupabaseClient) => {
-  const { data } = await supabase.from("summary").select("*");
+import { unstable_cache } from "next/cache";
+const getAllFromDB = unstable_cache(
+  async (supabase: SupabaseClient) => {
+    const { data } = await supabase.from("summary").select("*");
 
-  if (!data) {
-    throw new Error("No data found");
-  }
+    if (!data) {
+      throw new Error("No data found");
+    }
 
-  for (const summary of data) {
-    const youtubeVideoId = summary.youtube_video_id;
-    const title = await getYouTubeVideoTitle(youtubeVideoId);
-    summary.title = title;
-  }
+    for (const summary of data) {
+      const youtubeVideoId = summary.youtube_video_id;
+      const title = await getYouTubeVideoTitle(youtubeVideoId);
+      summary.title = title;
+    }
 
-  const camelCaseData = convertKeysToCamelCase(data);
-  const sortedData = camelCaseData.sort((a: any, b: any) => {
-    const aDate = new Date(a.createdAt);
-    const bDate = new Date(b.createdAt);
-    return aDate > bDate ? -1 : 1;
-  });
-  return sortedData;
-});
+    const camelCaseData = convertKeysToCamelCase(data);
+    const sortedData = camelCaseData.sort((a: any, b: any) => {
+      const aDate = new Date(a.createdAt);
+      const bDate = new Date(b.createdAt);
+      return aDate > bDate ? -1 : 1;
+    });
+    return sortedData;
+  },
+  [],
+  { revalidate: 2 }
+);
 
 export const getAllSummaries = async () => {
   const cookieStore = cookies();
