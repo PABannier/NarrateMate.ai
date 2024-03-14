5896647ba64a5a8a91bb317abacaac10ff55e5a7
Justina <justcho5@gmail.com>
2024-03-14 16:44:59 -0700

    initial table for words page

--- src/app/learning/review/words/page.tsx
+++ src/app/learning/review/words/page.tsx
@@ -1,12 +1,40 @@
-import PageHeader from "@/components/page-header";
-import React from "react";
+import { promises as fs } from "fs";
+import path from "path";
+import { Metadata } from "next";
+import Image from "next/image";
+import { z } from "zod";
 
-function WordsPage() {
-  return (
-    <div className="h-full px-4 py-6 lg:px-8 space-y-7">
-      <PageHeader title="Review" description="Review your saved words" />
-    </div>
+import { columns } from "./components/columns";
+import { DataTable } from "./components/data-table";
+import { UserNav } from "./components/user-nav";
+import { taskSchema } from "./data/schema";
+import PageHeader from "@/components/page-header";
+
+export const metadata: Metadata = {
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
+};
+
+// Simulate a database read for tasks.
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "./src/app/learning/review/words/data/tasks.json")
   );
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
 }
 
-export default WordsPage;
+export default async function WordsPage() {
+  const tasks = await getTasks();
+
+  return (
+    <>
+      <div className="h-full px-4 py-6 lg:px-8 space-y-7">
+        <PageHeader title="Words" description="Review your words" />
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  );
+}
