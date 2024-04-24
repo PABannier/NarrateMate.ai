b4b079b99286fc6fec94555a9ea97b036c5fbd06
Justina <justcho5@gmail.com>
2024-04-24 14:29:41 -0700

    dynamic url for dev vs prod

--- src/app/authentication/components/user-auth-form.tsx
+++ src/app/authentication/components/user-auth-form.tsx
@@ -12,8 +12,7 @@ import { useRouter } from "next/navigation";
 
 const getURL = () => {
   let url =
-    process.env.NODE_ENV === "development" ||
-    process.env.NODE_ENV === "production"
+    process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
       : process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
         process?.env?.NEXT_PUBLIC_VERCEL_URL ??
@@ -22,6 +21,7 @@ const getURL = () => {
   url = url.includes("http") ? url : `https://${url}`;
   // Make sure to include a trailing `/`.
   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+  console.log(url);
   return url;
 };
 console.log(getURL());
